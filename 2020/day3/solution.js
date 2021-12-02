9820c81a716b83b1fcbfa3ecefd643f4ee9b3804
Ziloka <50429450+Ziloka@users.noreply.github.com>
2021-12-02 20:15:38 +0000

    day3 example answer works but not actual answer

--- 2020/day3/solution.js
+++ 2020/day3/solution.js
@@ -1,38 +1,32 @@
 function getInput(){
-    // return `..##.......
-    //         #...#...#..
-    //         .#....#..#.
-    //         ..#.#...#.#
-    //         .#...##..#.
-    //         ..#.##.....
-    //         .#.#.#....#
-    //         .#........#
-    //         #.##...#...
-    //         #...##....#
-    //         .#..#...#.#`;
-    return require('fs').readFileSync('./input.txt', 'utf8');
+    return `..##.......
+            #...#...#..
+            .#....#..#.
+            ..#.#...#.#
+            .#...##..#.
+            ..#.##.....
+            .#.#.#....#
+            .#........#
+            #.##...#...
+            #...##....#
+            .#..#...#.#`;
+    // return require('fs').readFileSync('./input.txt', 'utf8');
 }
 
 function part1(arr){
     let result = 0;
-    let debug = 0;
     let previousXValue = 0; // starting x point
     for(let i=1;i<arr.length;i++){ // on each iteration the y position gets shifted by 1
         let str = arr[i];
         previousXValue = previousXValue + 3;// the x position to check
-        if(previousXValue >= str.length){ // check if we are unable to access character at specified position
+        if(previousXValue >= str.length){ // check if we are unable to access character at scified position
             // generate longer string so we can access the char position
             let temp = "";
-            for(let i=0;i<=previousXValue%str.length;i++){
+            for(let i=0;i<=Math.ceil(previousXValue/str.length);i++){ // identify least amount of iterations in order to access char position
                 temp = temp + str;
             }
             str = temp;
-
-            if(str.charAt(previousXValue) == ''){
-                console.log(`check line ${i} char pos ${previousXValue} str length ${str.length}`);
-            }
         }
-
         // access string
         if(str.charAt(previousXValue) == "#"){ // if at specific position was a tree, # is a tree
             result++;
@@ -42,7 +36,43 @@ function part1(arr){
 }
 
 function part2(arr){
-    
+    let result = 1;
+    let slopes = [
+        {
+            r: 1, d: 1
+        },{
+            r: 3, d: 1
+        },{
+            r: 5, d: 1
+        },{
+            r: 7, d: 1
+        },{
+            r: 1, d: 2
+        }
+    ];
+    // foreach slope
+    for(let slope of slopes){
+        let slopeResult = 0;
+        let previousXValue = 0; // starting x point
+        for(let i=1;i<arr.length;i=i+slope.d){ // on each iteration the y position gets shifted by 1
+            let str = arr[i];
+            previousXValue = previousXValue + slope.r;// the x position to check
+            if(previousXValue >= str.length){ // check if we are unable to access character at scified position
+                // generate longer string so we can access the char position
+                let temp = "";
+                for(let i=0;i<=Math.ceil(previousXValue/str.length);i++){ // identify least amount of iterations in order to access char position
+                    temp = temp + str;
+                }
+                str = temp;
+            }
+            // access string
+            if(str.charAt(previousXValue) == "#"){ // if at specific position was a tree, # is a tree
+                slopeResult++;
+            }
+        }
+        result = result * (slopeResult ?? 1);
+    }
+    return result;
 }
 
 const input = getInput();
