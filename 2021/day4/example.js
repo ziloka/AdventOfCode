e9f2d4b21c1e75fe5f4780b574c7ab1bd7ce5aa0
Ziloka <50429450+Ziloka@users.noreply.github.com>
2021-12-06 16:01:16 -0500

    finished 2021 day4 terrible performance ~250ms

--- 2021/day4/example.js
+++ 2021/day4/example.js
@@ -1,40 +1,36 @@
 // Example Solution
 
 function getInput() {
-  return `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+//   return `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
-22 13 17 11  0
-8  2 23  4 24
-21  9 14 16  7
-6 10  3 18  5
-1 12 20 15 19
+// 22 13 17 11  0
+// 8  2 23  4 24
+// 21  9 14 16  7
+// 6 10  3 18  5
+// 1 12 20 15 19
 
-3 15  0  2 22
-9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6
+// 3 15  0  2 22
+// 9 18 13 17  5
+// 19  8  7 25 23
+// 20 11 10 24  4
+// 14 21 16 12  6
 
-14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
-2  0 12  3  7`;
-  //return require("fs").readFileSync("input.txt", "utf8")
+// 14 21 17 24  4
+// 10 16 15  9 19
+// 18  8 23 26 20
+// 22 11 13  6  5
+// 2  0 12  3  7`;
+  return require("fs").readFileSync("input.txt", "utf8")
 }
 
-function part1(arr) {
-  // identify board that would win first0
-  let recur = (callNumbers, boards, marks, callPos) => {
-    // numbers that should be checked
-
-    let callNumber = callNumbers[callPos];
+ // identify board that would win first
+function getWinner(callNumbers, boards, marks, callPos){
+   // numbers that should be checked
+  let callNumber = callNumbers[callPos];
     for (let boardNumber = 0; boardNumber < boards.length; boardNumber++) {
       // checking if nums is inside board at specified position
       let tempMark = marks[boardNumber] ?? [];
-      for (let j = 0; j < boards[boardNumber].length; j++) {
-        tempMark[j] = tempMark[j] == true ? true : callNumber == boards[boardNumber][j];
-      }
+      for (let j = 0; j < boards[boardNumber].length; j++) tempMark[j] = tempMark[j] == true ? true : callNumber == boards[boardNumber][j];
       marks[boardNumber] = tempMark;
     }
 
@@ -62,82 +58,47 @@ function part1(arr) {
         break;
       }
     }
-    if (boardWinnerIndex >=0) {
+    if (boardWinnerIndex >= 0) {
       let board = boards[boardWinnerIndex];
-      let markedBoard = marks[boardWinnerIndex].map((v, i) => ({ index: i, value: v})).filter((e) => e.value == false).map((o) => o.index);
-
-      return board.filter((_, i) => markedBoard.includes(i)).reduce((a, b) => (+a)+(+b)) * callNumber;
+      let markedBoard = marks[boardWinnerIndex]
+        .map((v, i) => ({ index: i, value: v}))
+        .filter((e) => e.value == false)
+        .map((o) => o.index);
+      return {
+        index: boardWinnerIndex,
+        board: board.filter((_, i) => markedBoard.includes(i)),
+        marks: marks[boardWinnerIndex],
+        callNumber
+      };
     }
-    return recur(callNumbers, boards, marks, callPos + 1);
-  }
-  return recur(arr[0].split(","), arr.slice(1).map((s) => s.split(/\s+/gmi)), [], 0);
+    return getWinner(callNumbers, boards, marks, callPos + 1);
+}
+
+function part1(arr) {
+  let boardObj = getWinner(arr[0].split(","), arr.slice(1).map((s) => s.split(/\s+/gmi)), [], 0);
+  return boardObj.board
+    .filter((_, i) => boardObj.marks
+      .map((v, i) => ({ index: i, value: v}))
+      .filter((e) => e.value == false)
+      .map((o) => o.index))
+    .reduce((a, b) => (+a)+(+b)) * boardObj.callNumber;
 }
 
 function part2(arr) {
-  // identify board that would win last
-  let recur = (callNumbers, boards, marks, callPos, lastWinner) => {
-    // numbers that should be checked
-    let callNumber = callNumbers[callPos];
-    var lastWinnerPostions = lastWinner.map((v) => v.boardWinner);
-    for (let boardNumber = 0; boardNumber < boards.length; boardNumber++) {
-      // checking if nums is inside board at specified position
-    var lastWinnerPostions = lastWinner.map((v) => v.boardWinner);
-      if (!lastWinnerPostions.includes(boardNumber)) {
-      let tempMark = marks[boardNumber] ?? [];
-      for (let j = 0; j < boards[boardNumber].length; j++) {
-        tempMark[j] = tempMark[j] == true ? true : callNumber == boards[boardNumber][j];
-      }
-      marks[boardNumber] = tempMark;} 
-    }
-
-    // check if a board is a winner
-    let boardWinner = -1;
-    for (let i = 0; i < boards.length; i++) {
-      // identify board winner based off of index using markers array
-      let boardMarker = marks[i];
-       //if (i== 65) console.log(boardMarker);
-
-      if (!lastWinnerPostions.includes(i) && (
-        [0, 1, 2, 3, 4].every((e) => boardMarker[e]) ||
-        [5, 6, 7, 8, 9].every((e) => boardMarker[e]) ||
-        [10, 11, 12, 13, 14].every((e) => boardMarker[e]) ||
-        [15, 16, 17, 18, 19].every((e) => boardMarker[e]) ||
-        [20, 21, 22, 23, 24].every((e) => boardMarker[e]) ||
-        [0, 5, 10, 15, 20].every((e) => boardMarker[e]) ||
-        [1, 6, 11, 16, 21].every((e) => boardMarker[e]) ||
-        [2, 7, 12, 17, 22].every((e) => boardMarker[e]) ||
-        [3, 8, 13, 18, 23].every((e) => boardMarker[e]) ||
-        [4, 9, 14, 19, 24].every((e) => boardMarker[e]) ||
-        [0, 6, 12, 18, 24].every((e) => boardMarker[e]))
-      ) { // hardcode winnable board possibilites
-        console.log(i);
-        boardWinner = i;
-        break;
-      } 
-    }
-    if (boardWinner >=0) {
-      lastWinner.push({boardWinner, callNumber});
-      console.log(lastWinner);
-      //boards.splice(lastWinner, 1);
-    }
-
-    if(callPos == callNumbers.length-1){
-      let boardWinnerIndex = lastWinner[lastWinner.length-1].boardWinner;
-      let board = boards[boardWinnerIndex];
-      console.log(boardWinnerIndex);
-      console.log(marks);
-      let markedBoard = marks[boardWinnerIndex].map((v, i) => ({ index: i, value: v})).filter((e) => e.value == false).map((o) => o.index);
-      return board.filter((_, i) => markedBoard.includes(i)).reduce((a, b) => (+a)+(+b)) * lastWinner[lastWinner.length-1].callNumber; // exit 
-      return lastWinner;
-    }
-    return recur(callNumbers, boards, marks, callPos + 1, lastWinner);
+  let boards = arr.slice(1).map((s) => s.split(/\s+/gmi));
+  let lastWonBoard;
+  while(boards.length != 1){
+    let board = getWinner(arr[0].split(","), boards, [], 0);
+    boards.splice(board.index, 1);
   }
-  return recur(arr[0].split(","), arr.slice(1).map((s) => s.split(/\s+/gmi)), [], 0, []);
+  lastWonBoard = boards[0];
+  let wonBoard = getWinner(arr[0].split(","), [lastWonBoard], [], 0);
+  return wonBoard.board.reduce((a, b) => (+a)+(+b)) * wonBoard.callNumber;
 }
 
 let arr = getInput().split("\n\n").map((s) => s.trim());
 
 console.time();
-// console.log(`part 1: ${part1(arr)}`);
+console.log(`part 1: ${part1(arr)}`);
 console.log(`part 2: ${part2(arr)}`);
 console.timeEnd();
---
+++ 2021/day4/solution.py
@@ -0,0 +1,118 @@
+# python solution.py input.txt
+
+import pathlib
+import sys
+from typing import Any, TypeVar
+
+# Type annotation alias
+T = TypeVar('tuple[list[int], list[list[list[int]]]]')
+
+
+def intify(list_of_strings: list[str]) -> list[int]:
+  """ Convert a list of strings into one of ints """
+  return [int(c) for c in list_of_strings]
+
+
+def format_board(board: list[str]) -> list[int]:
+  """ Format each board from the input into int equivalents """
+  formatted_board = []
+  for row in board.splitlines():
+    row = row.replace('  ', ' ')
+    formatted_board.append(row.split())
+  return [intify(board) for board in formatted_board]
+
+
+def parse(puzzle_input: str) -> T:
+  """ Parse input """
+  data = puzzle_input.split('\n\n')
+  nums, boards = data[0].split(','), data[1:]
+  return (intify(nums), [format_board(board) for board in boards])
+
+
+def invert_board(board: list[list[int]]) -> list[list[int]]:
+  """ Switch rows and columns of board """
+  return [list(tupp) for tupp in list(zip(*board))]
+
+
+def winning_turns(nums: list[int], seq: list) -> int:
+  """ Return num of times it takes to match all nums in a sequence """
+  counter, turns = 0, 0
+  for num in nums:
+    turns += 1
+    if num in seq:
+      counter += 1
+      if counter == 5:
+        return turns
+
+
+def get_scores(board: list[list[int]], nums: list[int]) -> list[int]:
+    """ Return a list of winning turns for a board """
+    return [winning_turns(nums, seq) for seq in board]
+
+
+def scores(board: list[list[int]], nums: list[int]) -> list[int]:
+  """ Calculate scores for board and return all values as a list """
+  results = []
+  by_row = get_scores(board, nums)
+  by_col = get_scores(invert_board(board), nums)
+  for row, col in zip(by_row, by_col):
+    results.append(min(row, col))
+  return results
+
+
+def board_scores(boards: T, nums: list[int]) -> dict[int, list[int]]:
+  """ Get all the quickest scores for each board """
+  return {
+    board_no: min(scores(board, nums))
+    for board_no, board in enumerate(boards)
+  }
+
+
+def flatten(t: list[list[Any]]) -> list[Any]:
+  """ Flatten a nested list """
+  return [item for sublist in t for item in sublist]
+
+
+def final_score(board: list[list[int]], played_nums: list[int]) -> int:
+  """ Work out final score using sets """
+  last_num = played_nums[-1]
+  board_nums = set(flatten(board))
+  remaining_numbers = board_nums - set(played_nums)
+  return sum(remaining_numbers) * last_num
+
+
+def part1(data: T) -> int:
+  """ Solve part 1 """
+  nums, boards = data[0], data[1]
+  board_info = board_scores(boards, nums)
+  # Get quickest board
+  quickest = min(board_info, key=board_info.get)
+  num_turns = board_info[quickest]
+  return final_score(boards[quickest], nums[:num_turns])
+
+
+def part2(data: T) -> int:
+  """ Solve part 2 """
+  nums, boards = data[0], data[1]
+  board_info = board_scores(boards, nums)
+  # Get slowest board
+  slowest = max(board_info, key=board_info.get)
+  num_turns = board_info[slowest]
+  return final_score(boards[slowest], nums[:num_turns])
+
+
+def solve(puzzle_input: T) -> tuple[int, int]:
+  """ Solve the puzzle for the given input """
+  data = parse(puzzle_input)
+  solution1 = part1(data)  # Correct answer was 58412 (with my data)
+  solution2 = part2(data)  # Correct answer was 10030 (with my data)
+
+  return solution1, solution2
+
+
+if __name__ == "__main__":
+  for path in sys.argv[1:]:
+    print(f"{path}:")
+    puzzle_input = pathlib.Path(path).read_text().strip()
+    solutions = solve(puzzle_input)
+    print('\n'.join(str(solution) for solution in solutions))
