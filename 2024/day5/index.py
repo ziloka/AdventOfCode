96829dca0d4f370f9fddbc768501c9f86dbe89ce
Ziloka <50429450+ziloka@users.noreply.github.com>
2024-12-05 16:29:43 -0500

    2024 day 5 cleanup

--- 2024/day5/index.py
+++ 2024/day5/index.py
@@ -1,9 +1,11 @@
-puzzle_input = open("input.txt").read()
-page_ordering_rules, pages_to_produce = puzzle_input.split("\n\n")
-page_ordering_rules = [*map(lambda s: [*map(int, s.split("|"))], page_ordering_rules.split("\n"))]
-updates = [*map(lambda s: [*map(int, s.split(","))], pages_to_produce.split("\n"))]
+def parse(filename):
+    puzzle_input = open(filename).read()
+    page_ordering_rules, pages_to_produce = puzzle_input.split("\n\n")
+    page_ordering_rules = [*map(lambda s: [*map(int, s.split("|"))], page_ordering_rules.split("\n"))]
+    updates = [*map(lambda s: [*map(int, s.split(","))], pages_to_produce.split("\n"))]
+    return page_ordering_rules, updates
 
-def is_valid(page_nums):
+def is_valid(page_ordering_rules, page_nums):
     valid = True
     for num in page_nums:
         for rules in page_ordering_rules:
@@ -15,16 +17,14 @@ def is_valid(page_nums):
                     break
     return valid
 
-def part1():
+def part1(page_ordering_rules, updates):
     sum_middle_pages = 0
     for page_nums in updates:
-        if is_valid(page_nums):
+        if is_valid(page_ordering_rules, page_nums):
             sum_middle_pages += page_nums[len(page_nums) // 2]
     return sum_middle_pages
 
-print(f"part 1: {part1()}")
-
-def fix_page_nums(page_nums):
+def fix_page_nums(page_ordering_rules, page_nums):
     for i in range(len(page_nums)):
         for rules in page_ordering_rules:
             # perform selection sort, figure out first position, then second position, and so forth
@@ -33,14 +33,16 @@ def fix_page_nums(page_nums):
                 second = page_nums.index(rules[1])
                 if first > second:
                     page_nums[second], page_nums[first] = page_nums[first], page_nums[second]
-    return page_nums[::-1]
+    return page_nums
 
-def part2():
+def part2(page_ordering_rules, updates):
     sum_middle_pages = 0
     for page_nums in updates:
-        if not is_valid(page_nums):
-            page_nums = fix_page_nums(page_nums)
+        if not is_valid(page_ordering_rules, page_nums):
+            page_nums = fix_page_nums(page_ordering_rules, page_nums)
         sum_middle_pages += page_nums[len(page_nums) // 2]
     return sum_middle_pages
 
-print(f"part 2: {part2()}") # 11666 is too high
+args = parse("input.txt")
+print(f"part 1: {part1(*args)}")
+print(f"part 2: {part2(*args)}") # 11666 is too high
