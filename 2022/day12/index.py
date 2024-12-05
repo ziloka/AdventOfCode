e12d6ed41b82cd09ae4cf879082e04915f0815c9
Ziloka <50429450+ziloka@users.noreply.github.com>
2024-12-04 19:31:33 -0500

    2022 day 14 template

--- 2022/day12/index.py
+++ 2022/day12/index.py
@@ -1,8 +1,15 @@
+from enum import Enum
 import math
+
+class OBJECT(Enum):
+    ROCK = "#"
+    SAND = "o"
+    SOURCE = "+"
+    AIR = "."
 
 class Waterfall:
     def __init__(self):
-        sand_source = (500, 0)
+        self.sand_source = (500, 0)
 
         paths = []
         self.offset_x = math.inf
@@ -24,7 +31,7 @@ class Waterfall:
 
         self.paths = paths
         self.matrix = [["."] * (self.max_x-self.offset_x) for _ in range(self.max_y)]
-        self.place_object(sand_source, "+")
+        self.place_object(self.sand_source, OBJECT.SOURCE.value)
         self.place_rocks()
 
     def place_object(self, coords, object):
@@ -41,11 +48,11 @@ class Waterfall:
                     # x is from the right, so end_coord[0] < start_coord[0]
                     assert start_coord[1] < end_coord[1]
                     for y in range(start_coord[1], end_coord[1]+1):
-                        self.place_object((start_coord[0], y), "#")
+                        self.place_object((start_coord[0], y), OBJECT.ROCK.value)
                 elif start_coord[1] == end_coord[1]: # horizontal
-                    print("got here")
+                    assert end_coord[0] < start_coord[0]
                     for x in range(end_coord[0], start_coord[0]):
-                        self.place_object((x, start_coord[1]), "#")
+                        self.place_object((x, start_coord[1]), OBJECT.ROCK.value)
                 else:
                     raise Exception("Invalid path")
                 # self.debug()
@@ -56,8 +63,29 @@ class Waterfall:
             print("".join(line))
         print()
 
+    def sand_unit_movement(self, sand_unit):
+        while sand_unit[1] < self.max_y and self.matrix[sand_unit[1]+1][sand_unit[0]] == OBJECT.AIR.value:
+            sand_unit[1] += 1
+        # not free falling
+        # try moving one step down to the right
+        if self.matrix[sand_unit[1]+1][sand_unit[0]+1] == OBJECT.AIR.value: 
+            sand_unit[1] += 1
+            sand_unit[0] += 1
+        elif self.matrix[sand_unit[1]+1][sand_unit[0]-1] == OBJECT.AIR.value:
+            sand_unit[1] += 1
+            sand_unit[0] -= 1
+        return sand_unit
+
     def start_sand(self):
-        pass
+        counter = 0
+        while self.matrix[self.sand_source[1]-2][self.sand_source[0]] != OBJECT.SOURCE.value:
+            old_sand_unit = (self.sand_source[0]+1, self.sand_source[1])
+            new_sand_unit = self.sand_unit_movement(old_sand_unit)
+            if new_sand_unit == old_sand_unit: # sand at rest
+                self.place_object(new_sand_unit, OBJECT.SAND.value)
+            counter += 1
+        return counter
 
 waterfall = Waterfall()
 waterfall.debug()
+print(f"part 1: {waterfall.start_sand()}")
