aa8809ae457d584ac71be4d4aac3de5e41474567
Ziloka <50429450+ziloka@users.noreply.github.com>
2024-12-06 17:13:26 -0500

    2024 day 5 part 2 template

--- 2024/day6/index.py
+++ 2024/day6/index.py
@@ -12,6 +12,9 @@ class OBJECT(Enum):
 
 class Map():
     def __init__(self, filename="example.txt"):
+        self.init(filename)
+    
+    def init(self, filename):
         self.matrix = []
         self.visual = []
         rows = open(filename).read().split("\n")
@@ -50,36 +53,36 @@ class Map():
                 self.set_element(self.guard_pos, OBJECT.EMPTY.value)
                 self.set_visual_element(self.guard_pos, "X")
                 self.guard_pos[0] -= 1
-                positions.append(self.guard_pos.copy())
                 if not self.is_valid_pos(self.guard_pos):
-                    return None
+                    return [*positions, None]
+                positions.append(self.guard_pos.copy())
                 self.matrix[self.guard_pos[0]][self.guard_pos[1]] = GUARD.UP.value
         elif guard == GUARD.DOWN.value and self.get_element([self.guard_pos[0]+1, self.guard_pos[1]]) != OBJECT.OBSTRUCTION.value:
             while self.get_element([self.guard_pos[0]+1, self.guard_pos[1]]) != OBJECT.OBSTRUCTION.value:
                 self.set_element(self.guard_pos, OBJECT.EMPTY.value)
                 self.set_visual_element(self.guard_pos, "X")
                 self.guard_pos[0] += 1
-                positions.append(self.guard_pos.copy())
                 if not self.is_valid_pos(self.guard_pos):
-                    return None
+                    return [*positions, None]
+                positions.append(self.guard_pos.copy())
                 self.matrix[self.guard_pos[0]][self.guard_pos[1]] = GUARD.DOWN.value
         elif guard == GUARD.LEFT.value and self.get_element([self.guard_pos[0], self.guard_pos[1]-1]) != OBJECT.OBSTRUCTION.value:
             while self.get_element([self.guard_pos[0], self.guard_pos[1]-1]) != OBJECT.OBSTRUCTION.value:
                 self.set_element(self.guard_pos, OBJECT.EMPTY.value)
                 self.set_visual_element(self.guard_pos, "X")
                 self.guard_pos[1] -= 1
-                positions.append(self.guard_pos.copy())
                 if not self.is_valid_pos(self.guard_pos):
-                    return None
+                    return [*positions, None]
+                positions.append(self.guard_pos.copy())
                 self.matrix[self.guard_pos[0]][self.guard_pos[1]] = GUARD.LEFT.value
         elif guard == GUARD.RIGHT.value and self.get_element([self.guard_pos[0], self.guard_pos[1]+1]) != OBJECT.OBSTRUCTION.value:
             while self.get_element([self.guard_pos[0], self.guard_pos[1]+1]) != OBJECT.OBSTRUCTION.value:
                 self.set_element(self.guard_pos, OBJECT.EMPTY.value)
                 self.set_visual_element(self.guard_pos, "X")
                 self.guard_pos[1] += 1
-                positions.append(self.guard_pos.copy())
                 if not self.is_valid_pos(self.guard_pos):
-                    return None
+                    return [*positions, None]
+                positions.append(self.guard_pos.copy())
                 self.matrix[self.guard_pos[0]][self.guard_pos[1]] = GUARD.RIGHT.value
 
         # guard in front of obstruction
@@ -95,29 +98,61 @@ class Map():
             case _:
                 raise Exception("invalid guard")
         return positions
+    
+    def create_infinite_loops(self):
+        pass
         
     def part1(self):
         while True:
             positions = self.execute_protocol()
-            if positions == None:
+            if any([pos == None for pos in positions]):
                 break
         
+        original = open("expected.txt", "w")
         count = 0
-        for line in self.visual:
-            for element in line:
+        for i, line in enumerate(self.visual):
+            for j, element in enumerate(line):
                 if element == "X":
+                    original.write(f"{i} {j}\n")
                     count += 1
-        return count
-        # distinct_positions = set()
-        # while True:
-        #     positions = self.execute_protocol()
-        #     if positions == None:
-        #         break
-        #     # print(positions)
-        #     for pos in positions:
-        #         distinct_positions.add(f"{pos[0]}_{pos[1]}")
-        # # self.debug(self.visual)
-        # return len(distinct_positions)
+        
+        self.init("input.txt")
+
+        new = open("actual.txt", "w")
+        debug_positions = []
+        distinct_positions = set()
+        # https://stackoverflow.com/a/654002
+        while True:
+            positions = self.execute_protocol()
+            # print(positions)
+            for pos in positions:
+                if pos == None:
+                    break
+                if f"{pos[0]}_{pos[1]}" not in distinct_positions:
+                    debug_positions.append(pos)
+                distinct_positions.add(f"{pos[0]}_{pos[1]}")
+            else:
+                continue
+            break
+        # self.debug(self.visual)
+        # return len(distinct_positions) + 3
+
+        # https://stackoverflow.com/a/77446062
+        # debug_positions.sort(key=lambda l: l[0])
+        # sorting by x, then y
+        debug_positions = sorted(
+            sorted(
+                debug_positions, key = lambda x: x[1]
+            ),
+            key = lambda x: x[0]
+        )
+        for pos in debug_positions:
+            new.write(f"{pos[0]} {pos[1]}\n")
+    
+        return f"expected: {count}, actual: {len(distinct_positions)}"
+    
+    def part2(self):
+        pass
 
 map = Map("input.txt")
 # map.debug()
