3911491a108fe9c2b32d295487c158a86c26e126
Ziloka <50429450+ziloka@users.noreply.github.com>
2024-12-11 18:35:24 -0500

    2024 day 10 misread part 1

--- src/2024/day10/index.py
+++ src/2024/day10/index.py
@@ -11,34 +11,46 @@ class TopographicMap:
         for i, row_str in enumerate(topographic_list):
             for j, n in enumerate([*map(int, [*row_str])]):
                 self.map[i, j] = n
-                if n == 0:
-                    self.pos = (i, j)
 
     def is_valid(self, row, col):
         return 0 <= row < self.nums_row and 0 <= col < self.nums_col
 
-    def part1_helper(self, visited, pos, target = 1, result = 0):
-
-        # while (len(stack)):
-
-
-        visited[pos] = True
-        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]: # visit adjacent nodes
-            new_pos = (pos[0] + dr, pos[1] + dc)
-            if self.is_valid(*new_pos) and new_pos not in visited and self.map[new_pos] == target: # not visited, and is adjacent
-                if target == MAX_HEIGHT-1:
-                    result += 1
-                print(f"height: {target}, result: {result}")
-                self.part1_helper(visited, new_pos, target+1, result)
-
-        return result
-
     # iterative DFS approach
+    def get_trailheads(self, visited, pos):
+        target_height = 1
+        trailheads = 0
+        stack = [pos]
+        while len(stack):
+            pos = stack[-1]
+            stack.pop()
+            if pos not in visited:
+                print(f"visited {pos}, {self.map[pos]}")
+                if self.map[pos] == MAX_HEIGHT:
+                    print(f"trailhead {self.map[pos]}")
+                    trailheads += 1
+                target_height += 1
+                visited[pos] = True
+
+            for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]: # visit adjacent nodes
+                new_pos = (pos[0] + dr, pos[1] + dc)
+                if self.is_valid(*new_pos) and new_pos not in visited: # not visited
+                    if self.map[new_pos]+1 == target_height:
+                        stack.append(new_pos)
+        return trailheads
+
     def part1(self):
-        source = self.pos
-        visited = {}
-        result = self.part1_helper(visited, source)
-        return result
+        cumulative_score = 0
+        for i in range(self.nums_row):
+            for j in range(self.nums_col):
+                if self.map[i, j] == 0:
+                    source = (i, j)
+                    visited = {}
+                    trailheads = self.get_trailheads(visited, source)
+                    print(trailheads)
+                    print()
+                    # exit()
+                    cumulative_score += trailheads
+        return cumulative_score
     
 
     def part2(self):    
